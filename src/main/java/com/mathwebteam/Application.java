6c2566a7b30504b9aab7407918b6d7c499c2c10a
derker <893309018@qq.com>
2018-05-05 13:35:02 +0800

    init project

---
+++ src/main/java/com/mathwebteam/Application.java
@@ -0,0 +1,16 @@
+package com.mathwebteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author derker
+ * @date 2018-05-05
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/mathwebteam/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.mathwebteam.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author derker
+ * @date 2018-05-05
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
