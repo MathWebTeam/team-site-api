fe6974a50a56cd1776bf9730a20ff11a1123c076
derker <893309018@qq.com>
2018-05-05 14:34:54 +0800

    添加run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+pid=$(jps | grep team-site-api-1.0-SNAPSHOT.jar | awk '{print $1}')
+
+if [ $pid != "" ] ; then
+  kill -9 $pid
+fi
+
+nohup java -jar target/team-site-api-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
